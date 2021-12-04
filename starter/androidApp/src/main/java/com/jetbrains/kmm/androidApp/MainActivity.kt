c10d1d5334d7b4207a3775f24f00444e202b6a9f
Sunghun Park <mark.sunghun.park@gmail.com>
2021-12-04 19:49:25 +1100

    initial commit

---
+++ starter/androidApp/src/main/java/com/jetbrains/kmm/androidApp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jetbrains.kmm.androidApp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jetbrains.androidApp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
